22f34f3aa9e63b3fb82b6888ca2387b15b953f56
Mika Siddiqui <36973525+mikasiddiqui@users.noreply.github.com>
2026-02-17 23:36:58 +1100

    Fixed rendering

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,19 @@
+import type { NextConfig } from "next";
+
 const repo = "tofuautoworks";
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const isGithubSubpathBuild = process.env.DEPLOY_TARGET === "gh-pages";
+
+const nextConfig: NextConfig = {
   output: "export",
   distDir: "build",
   images: { unoptimized: true },
-
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}`,
   trailingSlash: true,
+  ...(isGithubSubpathBuild
+    ? {
+        basePath: `/${repo}`,
+        assetPrefix: `/${repo}`,
+      }
+    : {}),
 };
 
 export default nextConfig;
